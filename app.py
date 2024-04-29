92e27706f9afb00a44bf960c33c5324a5bbe5c4d
Medal Techie <37555807+medal007@users.noreply.github.com>
2024-04-29 13:17:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,28 +5,24 @@ import requests
 app = Flask(__name__)
 
 #from maytapi to integrate with api
-url = "https://api.maytapi.com/api/05236d2e-c3a9-4330-8b94-0f035a25b9c0/50857/sendMessage"
-payload = "{\"to_number\": \"+23278090384\",\"type\": \"text\",\"message\": \"Happy Birthday Bredda\"}"
-headers = {
-    'x-maytapi-key': "b5983ccc-a610-46a4-8674-3a83c6bbd41b",
-    'content-type': "application/json"
-    }
+# url = "https://api.maytapi.com/api/05236d2e-c3a9-4330-8b94-0f035a25b9c0/50857/sendMessage"
+# payload = "{\"to_number\": \"+23278090384\",\"type\": \"text\",\"message\": \"Happy Birthday Bredda\"}"
+# headers = {
+#     'x-maytapi-key': "b5983ccc-a610-46a4-8674-3a83c6bbd41b",
+#     'content-type': "application/json"
+#     }
 
-response = requests.request("POST", url, data=payload, headers=headers)
+# response = requests.request("POST", url, data=payload, headers=headers)
 
-print(response.text)
+# print(response.text)
 
 #webhook callback implementation
 
-
-
-
-
 # Define a route and a view function
-#@app.route('/', methods=['GET'])
-#def index():
- #   return "Welcome To Our Whatsapp Bot"
+@app.route('/', methods=['GET'])
+def index():
+   return "Welcome To Our Whatsapp Bot"
 
-# Run the app
-#if __name__ == '__main__':
- #   app.run(debug=True)
+Run the app
+if __name__ == '__main__':
+   app.run(debug=True)
